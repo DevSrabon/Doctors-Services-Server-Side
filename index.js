ef5a7abaf39d684fa76ef128008ea63c0ada0d50
devsrabon <et.srabon@gmail.com>
2022-11-09 12:57:24 +0600

    added services

--- index.js
+++ index.js
@@ -16,6 +16,32 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sajc8ea.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('docService').collection('services');
+          const orderCollection = client.db('docService').collection('orders');
+
+		
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+         app.get('/services/:id', async (req, res) => {
+						const id = req.params.id;
+						const query = { _id: ObjectId(id) };
+						const service = await serviceCollection.findOne(query);
+						res.send(service);
+					});
+
+    }
+    finally {
+        
+    }
+}
+run();
 
 app.listen(port, () => {
     console.log(`Genius car server running on ${port}`);
