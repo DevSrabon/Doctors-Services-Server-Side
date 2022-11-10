347c17c9536fb673c8498bb88f7ad68d120692a8
devsrabon <et.srabon@gmail.com>
2022-11-10 20:45:27 +0600

    added mongodb review

--- index.js
+++ index.js
@@ -6,49 +6,67 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('genius car server is running')
-})
+	res.send('genius car server is running');
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sajc8ea.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        const serviceCollection = client.db('docService').collection('services');
-          const orderCollection = client.db('docService').collection('orders');
+	try {
+		const serviceCollection = client.db('docService').collection('services');
+		const reviewCollection = client.db('docService').collection('reviews');
 
-		
-        app.get('/services', async (req, res) => {
-            const page = parseInt(req.query.page);
-            const size = parseInt(req.query.size);
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.skip(page*size).limit(size).toArray();
-            const count = await serviceCollection.estimatedDocumentCount();
-            res.send({services, count});
-        });
+		app.get('/services', async (req, res) => {
+			const page = parseInt(req.query.page);
+			const size = parseInt(req.query.size);
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor
+				.skip(page * size)
+				.limit(size)
+				.toArray();
+			const count = await serviceCollection.estimatedDocumentCount();
+			res.send({ services, count });
+		});
 
-         app.get('/services/:id', async (req, res) => {
-						const id = req.params.id;
-						const query = { _id: ObjectId(id) };
-						const service = await serviceCollection.findOne(query);
-						res.send(service);
-					});
-        app.post('/services', async (req, res) => {
-            const result = await serviceCollection.insertOne(req.body);
-                    })
+		app.get('/services/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const service = await serviceCollection.findOne(query);
+			res.send(service);
+		});
+		app.post('/services', async (req, res) => {
+			const result = await serviceCollection.insertOne(req.body);
+		});
+		app.get('/reviews', async (req, res) => {
+			const query = {};
+			const cursor = reviewCollection.find(query);
+			const reviews = await cursor.toArray();
+			res.send(reviews);
+		});
 
-    }
-    finally {
-        
-    }
+		app.post('/reviews', async (req, res) => {
+			const review = req.body;
+			const result = await reviewCollection.insertOne(review);
+			res.send(result);
+		});
+
+
+	} finally {
+	}
 }
 run();
 
 app.listen(port, () => {
-    console.log(`Genius car server running on ${port}`);
-})
+	console.log(`Genius car server running on ${port}`);
+});
